ecf792d2f2a551c4bee37673c906ec9250b5d806
Katie McBratney <kammcb14@gmail.com>
2020-04-21 19:59:23 -0700

    finished ui for multiplayer game - no computer

--- Board.java
+++ Board.java
@@ -5,113 +5,197 @@ public class Board
 {
     int[][] board = new int[8][8];
     String[] history = new String[62];
-    int[] currentPos1;
-    int[] currentPos2;
+    String firstPlayer;
+    String secondPlayer;
+    int currentPos1;
+    int currentPos2;
     int currentMove;
-    HashSet<int[]> legalMoves1 = new HashSet<int[]>();
-    HashSet<int[]> legalMoves2 = new HashSet<int[]>();
+    HashSet<Integer> legalMoves1;
+    HashSet<Integer> legalMoves2;
     //0 means empty, 1 means taken, 2 means the spot you are on, 3 is the 
     //spot the opponent is on
     boolean winner;// true is player 1
     public Board()
     {
-        board[0][0] = 2;
-        board[7][7] = 3;
-        currentPos1 = new int[] {0, 0};
-        currentPos2 = new int[] {7, 7};
-        currentMove = 0;
-    }
-    /*
-    public void play(boolean computerFirst)
-    {
-        if (computerFirst)
-        {
 
+    }
+    public void play()
+    {
+        boolean player1 = chooseFirstPlayer();
+        if(player1) {
+            board[0][0] = 2;
+            board[7][7] = 3;
+            currentPos1 = 11;
+            currentPos2 = 88;
+        } else{
+            board[0][0] = 3;
+            board[7][7] = 2;
+            currentPos1 = 88;
+            currentPos2 = 11;
         }
+        currentMove = 0;
+        legalMoves1 = getLegalMoves(currentPos1);
+        legalMoves2 = getLegalMoves(currentPos2);
+        while(!gameOver())
+        {
+            System.out.println("\f");
+            printBoard();
+            String newPosition = inputMove(player1);
+            move(newPosition, player1);
+            player1 = !player1;
+        }
+        if(legalMoves2.isEmpty()){
+            System.out.println("Player 1 is the Winner!");
+        }
+        else{
+            System.out.println("Player 2 is the Winner!");
+        }
+    }
+
+    private boolean chooseFirstPlayer()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Who goes first? 1 for Player 1, 2 for Player 2: ");
+        String player = sc.nextLine();
+            if (player.equals("1")){
+                firstPlayer = "Player 1";
+                secondPlayer = "Player 2";
+                return true;
+            }
+            else if (player.equals("2")) {
+                firstPlayer = "Player 2";
+                secondPlayer = "Player 1";
+                return false;
+            }
+            else
+            {
+                System.out.println("Entry Invalid. Try again");
+                return chooseFirstPlayer();
+            }
     }
 
     public boolean gameOver()
     {
-
+        try{
+            return legalMoves1.isEmpty() || legalMoves2.isEmpty();
+        }
+        catch (Exception e) {
+            return false;
+        }
     }
 
-    public boolean pieceStuck(boolean player1)
-    {
-
-    }
-    */
    //ok
     public void move(String inputPosition, boolean player1)
     {
-        if (!moveIsValid(inputPosition, player1))
+        int newPosition = stringToDescart(inputPosition);
+        if(!moveIsValid(newPosition, player1))
         {
             System.out.println("Move is not valid");
+            move(inputMove(player1), player1);
         }
-        else
-        {
-            int[] newPosition = stringToDescart(inputPosition);
+        else {
+            history[currentMove] = inputPosition;
             movePiece(newPosition, player1);
+            if(player1) legalMoves2 = getLegalMoves(currentPos2);
+            else legalMoves1 = getLegalMoves(currentPos1);
+            currentMove++;
         }
-        currentMove++;
     }
 //ok
     public String inputMove(boolean player1)
     {
-        System.out.print("Enter opponent's move: ");
+        if(player1){
+            System.out.println("Enter Player 1's move: ");
+        }
+        else System.out.println("Enter Player 2's move: ");
         Scanner sc = new Scanner(System.in);
         String move = sc.nextLine();
+        if(move.length()!=2){
+            System.out.println("Input invalid");
+            inputMove(player1);
+        }
         System.out.println();
         return move;
     }
 //ok
-    public void movePiece(int[] newPosition, boolean player1)
+    public void movePiece(int newPosition, boolean player1)
     {
         if (player1)
         {
-            board[currentPos1[0]][currentPos2[1]] = 1;
-            board[newPosition[0]][newPosition[1]] = 2;
             //moves piece to new position
-            currentPos1[0] = newPosition[0]; //record new position
-            currentPos1[1] = newPosition[1];
+            board[currentPos1/10 - 1][currentPos1%10 -1] = 1;
+            board[newPosition/10 -1][newPosition%10 -1] = 2;
+            //record new position
+            currentPos1 = newPosition;
         }
         else
         {
-            board[currentPos2[0]][currentPos2[1]] = 1;
-            board[newPosition[0]][newPosition[1]] = 3;
+            board[currentPos2/10 -1][currentPos2%10 -1] = 1;
+            board[newPosition/10 -1][newPosition%10 -1] = 3;
             //moves piece to new position
-            currentPos2[0] = newPosition[0]; //record new position
-            currentPos2[1] = newPosition[1];
+            currentPos2 = newPosition;
         }
+    }
+
+    public HashSet<Integer> getLegalMoves(int position){
+        HashSet<Integer> successors = new HashSet<>();
+        int row = position/10 -1;
+        int col = position%10 -1;
+        for(int i=col+1; i<board.length; i++) {
+            if (board[row][i] != 0) break;
+            successors.add((row+1)*10 + (i+1));
+        }
+        for(int i=col-1; i>=0; i--){
+            if(board[row][i] !=0)break;
+            successors.add((row+1)*10 + (i+1));
+        }
+        for(int i=row+1; i<board.length; i++){
+            if(board[i][col] != 0) break;
+            successors.add((i+1)*10 + (col+1));
+        }
+        for(int i=row-1; i>=0; i--){
+            if(board[i][col] !=0) break;
+            successors.add((i+1)*10 + (col+1));
+        }
+        int x = row+1;
+        int y= col+1;
+        while(x<board.length && y<board.length && board[x][y]==0){
+            successors.add((x+1)*10 + (y+1));
+            x++; y++;
+        }
+        x = row-1;
+        y= col+1;
+        while(x>=0 && y<board.length && board[x][y] ==0){
+            successors.add((x+1)*10 + (y+1));
+            x--; y++;
+        }
+        x = row+1;
+        y = col-1;
+        while(x<board.length && y>=0 && board[x][y]==0){
+            successors.add((x+1)*10 + (y+1));
+            x++; y--;
+        }
+        x = row-1;
+        y = col-1;
+        while(x>=0 && y>=0 && board[x][y]==0){
+            successors.add((x+1)*10 + (y+1));
+            x--; y--;
+        }
+        return successors;
     }
 
     //1 is player 1, 0 is player 2
     //tells you if move is valid
     //ok
-    public boolean moveIsValid(String inputPosition, boolean player1)
-    {
-        int[] inputCoordinates = stringToDescart(inputPosition);
-        int[] currentCoordinates;
-        currentCoordinates = player1 ? currentPos1: currentPos2;
-        if (inputCoordinates[0] == currentCoordinates[0] &&
-        inputCoordinates[1] == currentCoordinates[1])
-        {
+    public boolean moveIsValid(int inputPosition, boolean player1) {
+        int currentCoordinates;
+        currentCoordinates = player1 ? currentPos1 : currentPos2;
+
+        if (inputPosition == currentCoordinates) {
             return false;
         }
-
-        return isQueenMove(inputCoordinates, currentCoordinates);
-    }
-    //do this
-    public boolean isQueenMove(int[] input, int[] current)
-    {
-        if (input[0] == current[0] || input[1] == current[1])
-        {
-            return true;
-        }
-        else if (input[0] - current[0] == input[1] - current[1])
-        {
-            return true;
-        }
-        return false;
+        if(player1) return legalMoves1.contains(inputPosition);
+        else return legalMoves2.contains(inputPosition);
     }
 
     //tells you the value of the position on board
@@ -123,21 +207,22 @@ public class Board
     }
 
     //turns string into cartesian coordinates in a array of size 2
-    public static int[] stringToDescart(String position)
+    public static int stringToDescart(String position)
     {
         String columnStr = position.substring(1); 
         String rowStr = position.substring(0,1);
         int column = Integer.parseInt(columnStr) - 1;
         char r = rowStr.charAt(0);
         int row = r - 65;
-        int[] coordinates = {row, column};
+        int coordinates = (row+1)*10 + column+1;
         return coordinates;
     }
 
     public void printBoard()
     {
-        System.out.print( "  1 2 3 4 5 6 7 8\n");
+        System.out.print( "  1 2 3 4 5 6 7 8       "+ firstPlayer + " vs. "+secondPlayer +"\n");
         char letter;
+        int moveCounter = 0;
         for (int i = 0; i < board.length; i++)
         {
             if (i == 0)
@@ -168,6 +253,10 @@ public class Board
                 }
 
             }
+            if(moveCounter+1<=currentMove) System.out.print("     " + (moveCounter+1) + ". " + history[moveCounter]);
+            moveCounter++;
+            if(moveCounter+1<=currentMove) System.out.print("          " + (moveCounter+1) + ". " + history[moveCounter]);
+            moveCounter++;
             System.out.println();
         }
         System.out.println();
--- main.java
+++ main.java
@@ -1,42 +1,11 @@
-package IsolationAI;
-
 import java.util.Scanner;
 public class main
 {
     public static void main(String[] args)
     {
         Board b = new Board();
-        boolean computerFirst = chooseFirstPlayer(); 
-        
-            System.out.println("\f");
-            b.printBoard();
-            String newPosition = b.inputMove(computerFirst);
-            b.move(newPosition, false);
-            b.printBoard();
+        b.play();
         
     }
 
-    private static boolean chooseFirstPlayer()
-    {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Who goes first, C for Computer, O for opponent: ");
-        String firstPlayer = sc.nextLine();
-        boolean entered = false;
-        while (entered == false)
-        {
-            if (firstPlayer.equals("C"))
-            {
-                return true;
-            }
-            else if (firstPlayer.equals("O"))
-            {
-                return false;
-            }
-            else
-            {
-                System.out.println("Entry Invalid. Try again");
-            }
-        }
-        return false;
-    }
 }
